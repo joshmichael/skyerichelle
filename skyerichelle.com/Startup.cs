b6ed4f63aa99de68352e3b10eb0eaf93d2566c16
Josh <Josh@HILLTOPP_HP>
2017-05-03 19:03:17 -0700

    Add project files.

---
+++ skyerichelle.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(skyerichelle.com.Startup))]
+namespace skyerichelle.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
